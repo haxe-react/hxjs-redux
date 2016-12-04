e215b1a113624021c541fce7930800274d0169ea
Kevin Leung <kevinresol@gmail.com>
2016-12-04 21:25:57 +0800

    Compile fix

--- src/redux/Redux.hx
+++ src/redux/Redux.hx
@@ -6,10 +6,11 @@ import haxe.macro.Expr;
 @:jsRequire('redux')
 extern class Redux {
 	static function createStore<S, A:{type:String}>(reducer:S->A->S, ?initialState:S, ?enhancer:Function):Store<S, A>;
-	public static macro function combineReducers(e:Expr):Expr return macro redux.Reducer.combine($e);
 	
 	@:native('combineReducers') // the real one
 	static function _combineReducers<S, A:{type:String}>(reducers:{}):S->A->S;
+	
+	public static inline macro function combineReducers(e:Expr):Expr return macro redux.Reducer.combine($e);
 }
 
 extern class Store<S, A:{type:String}> {
